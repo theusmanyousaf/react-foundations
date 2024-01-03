37731a3a7d17f35c2092e84b9327227379d20222
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-01-03 22:38:38 +0500

    client component like button

--- app/page.jsx
+++ app/page.jsx
@@ -1,28 +1,21 @@
-import { useState } from 'react';
-
+import LikeButton from './like-button';
+ 
 function Header({ title }) {
   return <h1>{title ? title : 'Default title'}</h1>;
 }
-
+ 
 export default function HomePage() {
-  const names = ["Usman", "Hamza", "Faisal"];
-
-  const [likes, setLikes] = useState(0);
-
-  function handleClick() {
-    setLikes(likes + 1);
-  }
-
+  const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
+ 
   return (
     <div>
-      {/*Nesting a Header component*/}
       <Header title="Develop. Preview. Ship." />
       <ul>
         {names.map((name) => (
           <li key={name}>{name}</li>
         ))}
       </ul>
-      <button onClick={handleClick}>Like({likes})</button>
+      <LikeButton />
     </div>
   );
 }
